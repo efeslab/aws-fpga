a082bbe50bb49982c108750b6f13aae106a2b51f
Jiacheng Ma <jcma@umich.edu>
2021-08-19 18:34:16 -0400

    Add a valid bit to the writeback interface

--- hdk/cl/examples/cl_fpgarr/design/cl_fpgarr_writeback.sv
+++ hdk/cl/examples/cl_fpgarr/design/cl_fpgarr_writeback.sv
@@ -5,8 +5,10 @@ module rr_writeback #(
    input sync_rst_n,
    // cfg_max_payload: see https://github.com/aws/aws-fpga/blob/master/hdk/docs/AWS_Shell_Interface_Specification.md#pcim-interface----axi-4-for-outbound-pcie-transactions-cl-is-master-shell-is-slave-512-bit
    input logic [1:0] cfg_max_payload,
+   input logic din_valid,
+   output logic din_ready,
    input logic [WIDTH-1:0] din,
-   input logic [OFFSETWIDTH-1:0] offset,
+   input logic [OFFSETWIDTH-1:0] din_width,
    axi_bus_t.slave axi_out
 );
 // TODO: write log back to axi
