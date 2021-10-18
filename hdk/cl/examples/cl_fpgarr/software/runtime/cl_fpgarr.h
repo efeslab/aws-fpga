35242dde2b0c69b6a268d21a22eebf0ea82662f2
Gefei Zuo <gefeizuo@umich.edu>
2021-10-18 13:34:09 -0400

    Use MACRO to control fpgarr related CSRs.

---
+++ hdk/cl/examples/cl_fpgarr/software/runtime/cl_fpgarr.h
@@ -0,0 +1,11 @@
+#define CL_FPGARR_CSR_BASE 0x100000
+typedef enum {
+  BUF_ADDR_HI = 0,   // 0
+  BUF_ADDR_LO,       // 1
+  BUF_SIZE_HI,       // 2
+  BUF_SIZE_LO,       // 3
+  BUF_UPDATE,        // 4
+  FORCE_FINISH,      // 5
+  RR_MODE,           // 6
+} rr_csr_enum;
+#define RR_CSR_ADDR(idx) (CL_FPGARR_CSR_BASE + 0x4 * idx)
