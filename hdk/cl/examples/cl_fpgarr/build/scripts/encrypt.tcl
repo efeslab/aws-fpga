e03c2a09cad6d6d504b98421a29720836a63cc11
Gefei Zuo <gefeizuo@umich.edu>
2021-10-18 20:00:47 -0400

    Update synthesis scripts

--- hdk/cl/examples/cl_fpgarr/build/scripts/encrypt.tcl
+++ hdk/cl/examples/cl_fpgarr/build/scripts/encrypt.tcl
@@ -61,16 +61,23 @@ file copy -force $CL_DIR/design/cl_fpgarr_utils.sv                 $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_axi_rr.sv                $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_axil_rr.sv               $TARGET_DIR
 file copy -force $CL_DIR/design/twowayhandshake_logger.sv          $TARGET_DIR
+file copy -force $CL_DIR/design/twowayhandshake_replayer.sv        $TARGET_DIR
 file copy -force $CL_DIR/design/transkidbuf.sv                     $TARGET_DIR
 file copy -force $CL_DIR/design/transkidbuf_pipeline.sv            $TARGET_DIR
 file copy -force $CL_DIR/design/axichannel_logger.sv               $TARGET_DIR
+file copy -force $CL_DIR/design/axichannel_replayer.sv             $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_packing.sv               $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_packing_cfg.svh          $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_writeback.sv             $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_csrs.sv                  $TARGET_DIR
-file copy -force $CL_DIR/design/cl_fpgarr_wrapper.sv               $TARGET_DIR
 file copy -force $CL_DIR/design/cl_fpgarr_tracestorage_axi.sv      $TARGET_DIR
+file copy -force $CL_DIR/design/cl_fpgarr_tracedecoder.sv          $TARGET_DIR
+file copy -force $CL_DIR/design/cl_fpgarr_chgrouping.sv            $TARGET_DIR
+file copy -force $CL_DIR/design/cl_fpgarr_rr_sel.sv                $TARGET_DIR
+file copy -force $CL_DIR/design/cl_fpgarr_rt_loge_crossbar.sv      $TARGET_DIR
 file copy -force $CL_DIR/design/merged_fifo.sv                     $TARGET_DIR
+# fpgarr top module
+file copy -force $CL_DIR/design/cl_fpgarr_wrapper.sv               $TARGET_DIR
 
 #---- End of section replaced by Developr ---
 
