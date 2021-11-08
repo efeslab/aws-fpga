5a1e9802e601809b629325b5d9c15baee0e819a6
Gefei Zuo <gefeizuo@umich.edu>
2021-11-08 14:15:34 -0500

    Reduce RECORDER/REPLAYER_PIPE_DEPTH to reduce resource usage
    I was blindly incresing this pipe depth without experimenting what
    number is enough in terms of timing.
    I should reduce and start experimenting now.

--- hdk/cl/examples/cl_fpgarr/design/cl_fpgarr_defs.svh
+++ hdk/cl/examples/cl_fpgarr/design/cl_fpgarr_defs.svh
@@ -1,7 +1,7 @@
 `ifndef CL_FPGARR_DEFS
 `define CL_FPGARR_DEFS
-parameter RECORDER_PIPE_DEPTH=16;
-parameter REPLAYER_PIPE_DEPTH=8;
+parameter RECORDER_PIPE_DEPTH=4;
+parameter REPLAYER_PIPE_DEPTH=4;
 // index allocation of AXI channels
 parameter LOGB_AW=0;
 parameter LOGB_W=1;
