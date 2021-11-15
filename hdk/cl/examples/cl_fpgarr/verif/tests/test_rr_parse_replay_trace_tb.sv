259f8a80b1a6bd9cc060f573f219a1061018af34
Gefei Zuo <gefeizuo@umich.edu>
2021-11-15 00:52:14 -0500

    Integrate test_rr_parse_replay_trace with the record_bits and replay_bits CSR
    Now in the testbench,
    1. the end-of-record is controlled by the `do_finish` signal.
    2. the end-of-replay is controlled by the "replay_bits" register.

--- hdk/cl/examples/cl_fpgarr/verif/tests/test_rr_parse_replay_trace_tb.sv
+++ hdk/cl/examples/cl_fpgarr/verif/tests/test_rr_parse_replay_trace_tb.sv
@@ -8,7 +8,7 @@ module test_rr_parse_replay_trace_tb;
 // TODO: test all three gen rules
 `define PKT_GEN_RULE `PKT_GEN_RULE_BALANCE
 // TODO: Can we make TEST_BURST_LEN to test up to 200?
-localparam int TEST_BURST_LEN = 20;
+localparam int TEST_BURST_LEN = 100;
 localparam int NUM_BURST = 3;
 // end of TEST configuration
 
@@ -87,10 +87,25 @@ localparam int NUM_BURST = 3;
     logic replay_dout_ready, replay_dout_valid;
 
     logic finish;
-    logic [AXI_ADDR_WIDTH-1:0] buf_addr, buf_size;
+    logic record_finish;
+    logic [AXI_ADDR_WIDTH-1:0] read_addr, read_size, buf_addr, buf_size;
     logic write_buf_update, read_buf_update;
 
     logic write_interrupt, read_interrupt;
+    logic [63:0] record_bits, replay_bits;
+    logic [63:0] record_bits_old;
+    localparam bit [63:0] FAKE_INIT_REPLAY_BITS = 64'hffffffff;
+
+    // for record replay cross-check
+    logic [2*WIDTH-1:0] record_trace [NUM_BURST*TEST_BURST_LEN-1:0];
+    logic [2*WIDTH-1:0] replay_trace [NUM_BURST*TEST_BURST_LEN-1:0];
+    logic [OFFSET_WIDTH-1:0] record_len;
+    logic [OFFSET_WIDTH-1:0] replay_len;
+    int record_curr, replay_curr;
+    int len;
+
+    logic replay_dout_valid_ready_q;
+    // end of record replay cross-check
 
     rr_axi_bus_t axi_bus();
 
@@ -123,7 +138,9 @@ localparam int NUM_BURST = 3;
         .read_buf_size(read_size),
         .read_buf_update(read_buf_update),
         .write_interrupt(write_interrupt),
-        .read_interrupt(read_interrupt)
+        .read_interrupt(read_interrupt),
+        .record_bits(record_bits),
+        .replay_bits(replay_bits)
     );
 
     always begin
@@ -143,11 +160,13 @@ localparam int NUM_BURST = 3;
         record_din <= 0;
         record_din_width <= 0;
         record_din_valid <= 0;
+        record_finish <= 0;
         replay_dout_ready <= 1;
         axi_bus.awready <= 1;
         axi_bus.wready <= 1;
         axi_bus.arready <= 0;
         axi_bus.rvalid <= 0;
+        replay_bits <= FAKE_INIT_REPLAY_BITS;
         #period;
         for (int i = 0; i < 100; i++) begin
             #period;
@@ -169,7 +188,6 @@ localparam int NUM_BURST = 3;
         for (int i = 0; i < TEST_BURST_LEN; i++) begin
             tmp_packet = GET_RANDOM_PKT();
             tmp_packet_len = GET_LEN(tmp_packet[0 +: LOGB_CHANNEL_CNT]);
-            $display("[wb_tb]: record width %d data %x", tmp_packet_len, tmp_packet);
             record_din <= tmp_packet;
             record_din_width <= tmp_packet_len;
             record_din_valid <= 1;
@@ -185,7 +203,6 @@ localparam int NUM_BURST = 3;
         for (int i = 0; i < TEST_BURST_LEN; i++) begin
             tmp_packet = GET_RANDOM_PKT();
             tmp_packet_len = GET_LEN(tmp_packet[0 +: LOGB_CHANNEL_CNT]);
-            $display("[wb_tb]: record width %d data %x", tmp_packet_len, tmp_packet);
             record_din <= tmp_packet;
             record_din_width <= tmp_packet_len;
             record_din_valid <= 1;
@@ -194,6 +211,9 @@ localparam int NUM_BURST = 3;
               #period;
             end
         end
+        record_din <= 'hx;
+        record_din_width <= 'hx;
+        record_din_valid <= 0;
 
         replay_dout_ready <= 0;
         #period;
@@ -202,7 +222,6 @@ localparam int NUM_BURST = 3;
         for (int i = 0; i < TEST_BURST_LEN; i++) begin
             tmp_packet = GET_RANDOM_PKT();
             tmp_packet_len = GET_LEN(tmp_packet[0 +: LOGB_CHANNEL_CNT]);
-            $display("[wb_tb]: record width %d data %x", tmp_packet_len, tmp_packet);
             record_din <= tmp_packet;
             record_din_width <= tmp_packet_len;
             record_din_valid <= 1;
@@ -211,23 +230,22 @@ localparam int NUM_BURST = 3;
               #period;
             end
         end
-
-        // last packet, all zero
-        tmp_packet = 0;
-        tmp_packet_len = GET_LEN(tmp_packet[0 +: LOGB_CHANNEL_CNT]);
-        $display("[wb_tb]: last packet %d data %x", tmp_packet_len, tmp_packet);
-        record_din <= tmp_packet;
-        record_din_width <= tmp_packet_len;
-        record_din_valid <= 1;
-        #period;
-        while (~record_din_ready) begin
-          #period;
-        end
-        // end of last packet
-
-        record_din <= 0;
+        record_din <= 'hx;
+        record_din_width <= 'hx;
         record_din_valid <= 0;
+
+        record_bits_old <= record_bits;
+        #(period * 50);
+        while (record_bits_old != record_bits) begin
+            record_bits_old <= record_bits;
+            #(period * 50);
+        end
+        record_finish <= 1;
         #period;
+        record_finish <= 0;
+        #(period * 100);
+        replay_bits <= record_bits;
+
 
         for (int i = 0; i < 20; i++) begin
             #period;
@@ -242,18 +260,11 @@ localparam int NUM_BURST = 3;
         for (int i = 0; i < 200; i++) begin
             #period;
         end
+        $display("Record %d packets", record_curr);
+        $display("Replay %d packets", replay_curr);
 
         $finish;
     end
-
-    logic [2*WIDTH-1:0] record_trace [NUM_BURST*TEST_BURST_LEN-1:0];
-    logic [2*WIDTH-1:0] replay_trace [NUM_BURST*TEST_BURST_LEN-1:0];
-    logic [OFFSET_WIDTH-1:0] record_len;
-    logic [OFFSET_WIDTH-1:0] replay_len;
-    int record_curr, replay_curr;
-    int len;
-
-    logic replay_dout_valid_ready_q;
 
     initial begin
         record_curr = 0;
@@ -271,8 +282,8 @@ localparam int NUM_BURST = 3;
 
     always @(posedge clk) begin
         if (replay_dout_valid && replay_dout_ready) begin
-            $display("[wb_tb@%t]: replay width %d (+width %d aligned to %d) data %x",
-                $time,
+            $display("[wb_tb@%t]: #%d replay width %d (+width %d aligned to %d) data %x",
+                $time, replay_curr,
                 GET_LEN(replay_dout[0 +: LOGB_CHANNEL_CNT]),
                 GET_LEN(replay_dout[0 +: LOGB_CHANNEL_CNT]) + OFFSET_WIDTH'(OFFSET_WIDTH),
                 replay_dout_width, replay_dout);
@@ -284,6 +295,8 @@ localparam int NUM_BURST = 3;
             record_trace[record_curr][0 +: WIDTH] <= record_din;
             record_trace[record_curr][WIDTH +: WIDTH] <= 0;
             record_curr <= record_curr + 1;
+            $display("[wb_tb]: #%d record width %d  data %x",
+                record_curr, record_din_width, record_din);
         end
 
         if (replay_dout_valid && replay_dout_ready) begin
