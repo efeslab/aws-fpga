7fa3d20f275d6ec4e539f5ab41ce94c3230834b2
Jiacheng Ma <jcma@umich.edu>
2021-12-03 22:34:18 -0500

    add synthesis script

--- hdk/cl/examples/cl_3d_rendering/build/scripts/encrypt.tcl
+++ hdk/cl/examples/cl_3d_rendering/build/scripts/encrypt.tcl
@@ -35,23 +35,43 @@ if {[llength [glob -nocomplain -dir $TARGET_DIR *]] != 0} {
 
 ## Change file names and paths below to reflect your CL area.  DO NOT include AWS RTL files.
 file copy -force $CL_DIR/../common/design/cl_common_defines.vh     $TARGET_DIR
+file copy -force $CL_DIR/design/cl_dma_pcis_slv.sv                 $TARGET_DIR
 file copy -force $CL_DIR/design/cl_dram_dma_defines.vh             $TARGET_DIR
-file copy -force $CL_DIR/design/cl_id_defines.vh                   $TARGET_DIR
 file copy -force $CL_DIR/design/cl_dram_dma_pkg.sv                 $TARGET_DIR
 file copy -force $CL_DIR/design/cl_dram_dma.sv                     $TARGET_DIR
-file copy -force $CL_DIR/design/cl_tst.sv                          $TARGET_DIR
-file copy -force $CL_DIR/design/cl_int_tst.sv                      $TARGET_DIR
-file copy -force $CL_DIR/design/mem_scrb.sv                        $TARGET_DIR
-file copy -force $CL_DIR/design/cl_tst_scrb.sv                     $TARGET_DIR
-file copy -force $CL_DIR/design/axil_slave.sv                      $TARGET_DIR
-file copy -force $CL_DIR/design/cl_int_slv.sv                      $TARGET_DIR
-file copy -force $CL_DIR/design/cl_pcim_mstr.sv                    $TARGET_DIR
-file copy -force $CL_DIR/design/cl_vio.sv                          $TARGET_DIR
-file copy -force $CL_DIR/design/cl_dma_pcis_slv.sv                 $TARGET_DIR
-file copy -force $CL_DIR/design/cl_ila.sv                          $TARGET_DIR
-file copy -force $CL_DIR/design/cl_ocl_slv.sv                      $TARGET_DIR
-file copy -force $CL_DIR/design/cl_sda_slv.sv                      $TARGET_DIR
-file copy -force $CL_DIR/design/cl_dram_dma_axi_mstr.sv            $TARGET_DIR
+file copy -force $CL_DIR/design/cl_id_defines.vh                   $TARGET_DIR
+file copy -force $CL_DIR/design/cl_ocl_slv_reg.sv                  $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel_wrapper.sv               $TARGET_DIR
+
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_coloringFB.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_control_s_axi.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_in_loop_TRIANGLES_fragment_color_V_memcore.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_in_loop_TRIANGLES_fragment_color_V.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_in_loop_TRIANGLES_fragment_x_V_memcore.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_in_loop_TRIANGLES_fragment_x_V.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_in_loop_TRIANGLES.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_dataflow_parent_loop_proc.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_fifo_w12_d2_S.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_fifo_w12_d4_S.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_fifo_w16_d2_S.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_fifo_w1_d2_S.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_fifo_w8_d2_S.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_frame_buffer_V.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_gmem_m_axi.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_mac_mulsub_9s_9s_18ns_18_4_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_mul_8ns_10ns_17_1_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_mul_9s_9s_16_1_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_mul_9s_9s_18_1_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_projection.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_rasterization1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_rasterization2.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_udiv_16ns_8ns_8_20_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_urem_16ns_8ns_8_20_1.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_zculling.v $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_zculling_z_buffer_V_ram.dat $TARGET_DIR
+file copy -force $CL_DIR/design/hls_accel/verilog/rendering_zculling_z_buffer_V.v $TARGET_DIR
+
 file copy -force $UNUSED_TEMPLATES_DIR/unused_sh_bar1_template.inc $TARGET_DIR
 
 #---- End of section replaced by Developr ---
