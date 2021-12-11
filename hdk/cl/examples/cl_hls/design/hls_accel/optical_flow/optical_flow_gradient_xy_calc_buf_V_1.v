063feba4b8aadb409ae3bba9d2cb2a607f42e171
Jiacheng Ma <jcma@umich.edu>
2021-12-11 00:58:26 -0500

    [benchmark] add optical flow

---
+++ hdk/cl/examples/cl_hls/design/hls_accel/optical_flow/optical_flow_gradient_xy_calc_buf_V_1.v
@@ -0,0 +1,71 @@
+// ==============================================================
+// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// ==============================================================
+`timescale 1 ns / 1 ps
+module optical_flow_gradient_xy_calc_buf_V_1_ram (addr0, ce0, d0, we0, q0,  clk);
+
+parameter DWIDTH = 28;
+parameter AWIDTH = 10;
+parameter MEM_SIZE = 1024;
+
+input[AWIDTH-1:0] addr0;
+input ce0;
+input[DWIDTH-1:0] d0;
+input we0;
+output reg[DWIDTH-1:0] q0;
+input clk;
+
+reg [DWIDTH-1:0] ram[0:MEM_SIZE-1];
+
+initial begin
+    $readmemh("./optical_flow_gradient_xy_calc_buf_V_1_ram.dat", ram);
+end
+
+
+
+always @(posedge clk)  
+begin 
+    if (ce0) begin
+        if (we0) 
+            ram[addr0] <= d0; 
+        q0 <= ram[addr0];
+    end
+end
+
+
+endmodule
+
+`timescale 1 ns / 1 ps
+module optical_flow_gradient_xy_calc_buf_V_1(
+    reset,
+    clk,
+    address0,
+    ce0,
+    we0,
+    d0,
+    q0);
+
+parameter DataWidth = 32'd28;
+parameter AddressRange = 32'd1024;
+parameter AddressWidth = 32'd10;
+input reset;
+input clk;
+input[AddressWidth - 1:0] address0;
+input ce0;
+input we0;
+input[DataWidth - 1:0] d0;
+output[DataWidth - 1:0] q0;
+
+
+
+optical_flow_gradient_xy_calc_buf_V_1_ram optical_flow_gradient_xy_calc_buf_V_1_ram_U(
+    .clk( clk ),
+    .addr0( address0 ),
+    .ce0( ce0 ),
+    .we0( we0 ),
+    .d0( d0 ),
+    .q0( q0 ));
+
+endmodule
+
