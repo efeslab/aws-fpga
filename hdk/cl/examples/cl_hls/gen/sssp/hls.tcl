07dcca86b6f81ae8f9370d4ebe005c4880f3e798
Jiacheng Ma <jcma@umich.edu>
2021-12-12 00:29:18 -0500

    [benchmark] add sssp data

---
+++ hdk/cl/examples/cl_hls/gen/sssp/hls.tcl
@@ -0,0 +1,15 @@
+open_project -reset proj
+
+add_files bf_kernel.cl
+
+set_top bellman_ford
+
+open_solution -reset -flow_target vitis solution
+
+set_part "xcvu9p-flgb2104-2-i"
+
+create_clock -period "250MHz"
+
+config_interface -m_axi_addr64 -m_axi_max_bitwidth 512 -m_axi_min_bitwidth 512 -m_axi_max_read_burst_length 16 -m_axi_max_write_burst_length 16
+
+csynth_design
