b5775a19bbbcb7c2e6783aa890efbbb4d11cb3cc
Gefei Zuo <gefeizuo@umich.edu>
2022-05-13 18:18:48 -0400

    Add synthesis scripts and notes for nbody-simulation

--- hdk/cl/examples/cl_hls/README.md
+++ hdk/cl/examples/cl_hls/README.md
@@ -1,3 +1,9 @@
 # CL_HLS FPGARR benchmark
 Remember to patch `Vitis_HLS_2020.2_include.patch` before generating RTL from
 the HLS.
+
+# About HLS Generation
+For simulation, use the `export_design` to export a zip then extract.
+For synthesis, pay attention to .tcl files related to ip that locate inside
+the `$project/solution/syn/verilog` directory. source those tcl in the synthesis
+scripts.
---
+++ hdk/cl/examples/cl_hls/build/scripts/nbody_2CU2Pipe_ip.tcl
@@ -0,0 +1,6 @@
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_fadd_5_full_dsp_32_ip.tcl
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_faddfsub_5_full_dsp_32_ip.tcl
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_fdiv_10_no_dsp_32_ip.tcl
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_fmul_2_max_dsp_32_ip.tcl
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_fsqrt_10_no_dsp_32_ip.tcl
+source $CL_DIR/design/hls_accel/nbody_2CU2Pipe/hdl/ip/ipgen/nbody_ap_fsub_5_full_dsp_32_ip.tcl
